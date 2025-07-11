fdec161c02e8987ce3f723a95b253f1df7640cca
elogio.mi@gmail.com <elogio.mi@gmail.com>
2025-07-11 21:46:56 +0900

    [FIX]  빌드오류로 인한 import 파일명 변경

--- src/components/ReservationModal.jsx
+++ src/components/ReservationModal.jsx
@@ -3,7 +3,7 @@
 import { formatDate } from "@/lib/utils"
 import { useState, useEffect } from "react"
 import { Clock, X } from "lucide-react"
-import { Button } from "@/components/ui/Button"
+import { Button } from "@/components/ui/Button.jsx"
 import {
   Dialog,
   DialogContent,
