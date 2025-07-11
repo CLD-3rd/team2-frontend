fea23ccf1b66bb01b4313a3250f4e1f049fd5f6e
elogio.mi@gmail.com <elogio.mi@gmail.com>
2025-07-11 15:10:37 +0900

    [FIX] 서버요청시 쿠키 보내는 조건 수정

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,8 +7,10 @@ const API_BASE_URL = `${import.meta.env.VITE_SERVER_URL}/api`
 async function apiRequest(endpoint, options = {}) {
   const url = `${API_BASE_URL}${endpoint}`
   
+   const isLoggedIn = !!getAuthToken() && !isTokenExpired(getAuthToken())
+
   const defaultOptions = {
-    credentials: "include",
+    ...(isLoggedIn && { credentials: "include" }),
     headers: {
       'Content-Type': 'application/json',
       ...options.headers,
